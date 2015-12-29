cd6de6cda54878a20a2073a94afa7570af0fc6ba
linhly2012 <sarahlee@Linhs-MBP.PK5001Z>
2015-12-29 12:01:29 -0800

    #1..general look

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  tipsCalCulator
+//
+//  Created by Linh Ly on 12/4/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipsLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
